3a80013875ca123549e480d0c0841c771348e683
javier-camilo <camiloviloriatovar@gmail.com>
2020-10-11 09:53:55 -0500

    nuevas operaciones

--- Notas/app/src/main/java/com/example/notas/MainActivity.java
+++ Notas/app/src/main/java/com/example/notas/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println(mostrarAca);
+        System.out.println("juego");
     }
 
 
